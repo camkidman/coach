b0dc36d8df46f32f697beb96a8e47007d5a532e5
Cam Kidman <cameronjkidman@gmail.com>
2016-07-24 13:29:52 -0600

    Add user dashboard for users to see their own feedback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def dashboard
+    @user = User.find(params[:user_id])
+    @polls = Poll.where(:user_id => params[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, :only => [:index] do
     resources :polls
+    get "dashboard"
   end
   get "users_list", :to => "users#index"
   devise_for :users
